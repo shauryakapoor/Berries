33801fd04642af4d08b2174c0c6b9c6dea6ca29c
Kevin Crabbe <kevincrabbe@Kevins-MBP-2.attlocal.net>
2021-10-18 20:36:22 -0700

    added websocket dependency

---
+++ src/main/java/com/berries/berries/WebSocketConfig.java
@@ -0,0 +1,10 @@
+package com.berries.berries;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig {
+}
