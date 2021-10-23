a011d1242c544cb16274ae1b4bd461ddc62b315a
Kevin Crabbe <kevincrabbe@Kevins-MBP-2.attlocal.net>
2021-10-22 18:42:52 -0700

    started to implement getChatId

--- src/main/java/com/berries/berries/Repository/ChatRoomRepository.java
+++ src/main/java/com/berries/berries/Repository/ChatRoomRepository.java
@@ -2,9 +2,11 @@ package com.berries.berries.Repository;
 
 import com.berries.berries.Entity.ChatRoomEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ChatRoomRepository extends MongoRepository<ChatRoomEntity, String> {
     Optional<ChatRoomEntity> findBySenderIdAndRecipientId(String senderId, String recipientId);
 }
--- src/main/java/com/berries/berries/Service/ChatRoomService.java
+++ src/main/java/com/berries/berries/Service/ChatRoomService.java
@@ -1,9 +1,24 @@
 package com.berries.berries.Service;
 
+import com.berries.berries.Entity.ChatRoomEntity;
+import com.berries.berries.Repository.ChatRoomRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ChatRoomService {
     ChatRoomRepository chatRoomRepository;
 
+    public Optional<String> getChatId(String senderId, String recipientId, boolean createIfNotExist) {
+        Optional<ChatRoomEntity> optionalChatRoomEntity =
+                chatRoomRepository.findBySenderIdAndRecipientId(senderId, recipientId);
+
+        Optional<String> chatId = optionalChatRoomEntity.map(ChatRoomEntity::getChatId)
+                .or(this::createChatRoom);
+    }
+
+    private Optional<String> createChatRoom() {
+        return Optional.of("");
+    }
 }
